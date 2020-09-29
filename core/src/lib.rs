732fb02a0b19679db9e2dfcd7467dd1c4a07fd59
GuangZe <haofu@91jkys.com>
2020-09-29 18:13:09 +0800

    comment add

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -13,5 +13,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// 1.skywalking的proto协议中没有package的设置，但是tonic强制要求必须
+// 在proto中定义package的信息;
+// 2.Trace接口中的 collect方法和集合中的方法发生重合导致项目编译提示问题
+// 综合上述两点：
+// 直接采用生成的rust grp client代码嵌入到项目中的方式
+//
+// pub mod remote {
+//     tonic::include_proto!("remote");
+// }
 mod remote;
 pub mod skywalking;
